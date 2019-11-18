a6ec237bb2a2df597f6e0c51372369262de43b2a
nhlDailyFantasy <amense41@gmail.com>
2019-11-18 13:16:04 -0600

    Revert "toolbar"
    This reverts commit b47ef0673fa161a96563a4b46b761a6b75cf6a33.

--- nhldailyfantasy/src/App.js
+++ nhldailyfantasy/src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import { Layout, useLayoutNavigation } from 'react-md';
+import logo from './logo.svg';
 import './App.css';
-import './App.scss';
-
-const navItems = {};
 
 function App() {
   return (
-    <Layout
-      {...useLayoutNavigation(navItems, window.location.pathname)}
-      appBarTitle="react-md App"
-      navHeaderTitle="My App"
-    >
-      <div className="App">
-        <div>Team</div>
-        <div>Player</div>
-        <div>Standings</div>
-        <div>Daily</div>
-      </div>
-    </Layout>
+    <div className="App">
+
+    </div>
   );
 }
 
--- nhldailyfantasy/src/index.js
+++ nhldailyfantasy/src/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Configuration } from 'react-md';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <Configuration>
-    <App />
-  </Configuration>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
